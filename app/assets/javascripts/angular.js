72736df3ae675d5d0c675416dec5a2b3ac9d39d2
garonbailey <bailegm@gmail.com>
2015-12-07 15:02:49 -0500

    angular routing, angular templates, angular controller, and basics of the template all set up

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -2,8 +2,15 @@ var app = angular.module('MoodAdjuster', ['ngRoute']);
 
 app.controller('apiCtrl', ['$routeParams', '$http', function ($routeParams, $http) {
 	this.message = "I'm gonna show up on the screen";
-	console.log("I'm up and running");
 }]);
+
+app.controller('searchCtrl', ['$routeParams', '$http', function ($routeParams, $http) {
+	this.message = "Search Page; Form goes here!"
+}]);
+
+app.controller('resultCtrl', ['$routeParams', '$http', function ($routeParams, $http) {
+	this.message = "Result Page; Auto re-direct?"
+}])
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 	$locationProvider.html5Mode({ enabled: true });
@@ -11,6 +18,16 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 		when('/', {
 			templateUrl: '/angular_templates/index.html',
 			controller: 'apiCtrl',
+			controllerAs: 'ctrl'
+		}).
+		when('/search', {
+			templateUrl:  '/angular_templates/search.html',
+			controller: 'searchCtrl',
+			controllerAs: 'ctrl'
+		}).
+		when('/result', {
+			templateUrl: '/angular_templates/result.html',
+			controller: 'resultCtrl',
 			controllerAs: 'ctrl'
 		}).
 		otherwise({
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
 
+  get '/' => 'application#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
