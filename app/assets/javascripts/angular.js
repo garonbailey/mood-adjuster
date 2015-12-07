4ae2c6044aba537b6177076076287f605922ff97
garonbailey <bailegm@gmail.com>
2015-12-07 17:30:00 -0500

    fixed search form, updated css for testing purposes, worked out search logic/else if statements in angular controller for api search

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -4,12 +4,135 @@ app.controller('apiCtrl', ['$routeParams', '$http', function ($routeParams, $htt
 	this.message = "I'm gonna show up on the screen";
 }]);
 
-app.controller('searchCtrl', ['$routeParams', '$http', function ($routeParams, $http) {
-	this.message = "Search Page; Form goes here!"
+app.controller('searchCtrl', ['$routeParams', '$http', '$scope', function ($routeParams, $http, $scope) {
+	this.message = "Search Page; Form goes here!";
+
+	var controller = this;
+	controller.search = function () {
+		var mood = controller.mood;
+		var action = controller.change;
+
+		if (action === "Reinforce My Mood") {
+			if (mood === "Surprise") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			}
+		} else {
+			if (mood === "Sad") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=happy&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Happy") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=miserable&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Angry") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=peaceful&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Content") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=upset&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Nervous") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=calm&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Edgy") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=laid-back&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Annoyed") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=pleased&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Calm") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=agitated&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Relaxed") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/search?q=tense&api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			} else if (mood === "Surprise") {
+				$http({
+					method: 'GET',
+					url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
+				}).then(function success(res) {
+					console.log(res.data);
+				}, function error(res) {
+					controller.message = "Error processing request"
+				});
+			}
+		}
+
+		console.log("Selected Mood: ", mood);
+		console.log("Selected Action: ", action);
+	};
 }]);
 
 app.controller('resultCtrl', ['$routeParams', '$http', function ($routeParams, $http) {
-	this.message = "Result Page; Auto re-direct?"
+	this.message = "Result Page; Auto re-direct here?"
 }])
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
@@ -21,7 +144,7 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 			controllerAs: 'ctrl'
 		}).
 		when('/search', {
-			templateUrl:  '/angular_templates/search.html',
+			templateUrl:  '/angular_templates/searchin.html',
 			controller: 'searchCtrl',
 			controllerAs: 'ctrl'
 		}).
