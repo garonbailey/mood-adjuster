56e04ce23cbf10f80eeb28c046af1772239c439c
garonbailey <bailegm@gmail.com>
2015-12-11 20:04:18 -0500

    got hovered footer working, styled result, almost complete

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,13 @@
-$('footer').click(function() {
-    $('div.subfoot').removeClass("hidden");
-  });
+$(document).ready(function () {
+	$('.footer').hover(function() {
+	    $('div.subfoot').removeClass("hidden");
+	  }, function () {
+	  	$('div.subfoot').addClass("hidden");
+	  });
 
-$('div.subfoot').hover(function() {
-    $(this).removeClass("hidden");
-  }, function() {
-    $(this).addClass("hidden");
+	$('div.subfoot').hover(function() {
+	    $(this).removeClass("hidden");
+	  }, function() {
+	    $(this).addClass("hidden");
   });
+});
