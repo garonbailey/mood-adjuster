60b2854d9dd253e2fa8b7aec2cb0edbe1f55dbb6
garonbailey <bailegm@gmail.com>
2015-12-11 21:06:14 -0500

    fixed api call issue, I hope

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -4,7 +4,7 @@ app.controller('apiCtrl', ['$routeParams', '$http', function ($routeParams, $htt
 	var controller = this;
 	$http({
 		method: 'GET',
-		url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
+		url: "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
 	}).then(function success(res) {
 		controller.gif = res.data.data.image_url;
 		console.log(controller.gif);
@@ -26,7 +26,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			if (mood === "Surprise") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data.image_url;
 					controller.message = "How does it make you feel?";
@@ -36,7 +36,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "Hope this is exactly the appeasement you're looking for.";
@@ -48,7 +48,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			if (mood === "Sad") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=happy&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=happy&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "Hope you feel better, buddy!";
@@ -58,7 +58,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Happy") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=miserable&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=miserable&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "Did we successfully ruin your happiness, weirdo?";
@@ -68,7 +68,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Angry") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=peaceful&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=peaceful&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "Just tryin' to help you chill out, dude.";
@@ -78,7 +78,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Content") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=upset&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=upset&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "Did we successfully ruin your contented feelings?";
@@ -88,7 +88,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Nervous") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=calm&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=calm&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "Here's something to calm your nerves.";
@@ -98,7 +98,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Edgy") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=laid-back&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=laid-back&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "A little something to take the edge off.";
@@ -108,7 +108,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Annoyed") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=pleased&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=pleased&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "Just breathe . . . and check out this gif.";
@@ -118,7 +118,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Calm") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=agitated&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=agitated&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "I won't judge your lifestyle. Hope this successfully stresses you out.";
@@ -128,7 +128,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Relaxed") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/search?q=tense&api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/search?q=tense&api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data[Math.floor(Math.random()*25)].images.original.url;
 					controller.message = "Being relaxed is for losers! Rage!";
@@ -138,7 +138,7 @@ app.controller('searchCtrl', ['$routeParams', '$http', '$scope', '$location', fu
 			} else if (mood === "Surprise") {
 				$http({
 					method: 'GET',
-					url: "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
+					url: "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC"
 				}).then(function success(res) {
 					controller.gifUrl = res.data.data.image_url;
 					controller.message = "Surprise! It's a random gif!";
